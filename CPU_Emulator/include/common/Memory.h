f6e714349d733b2f91e0225fdbf4eaf17b1e2017
SpinnerX <heraaronhotmail@yahoo.com>
2023-09-12 06:04:22 +0000

    Initial commit

--- CPU_Emulator/include/common/Memory.h
+++ CPU_Emulator/include/common/Memory.h
@@ -1,7 +1,32 @@
 #pragma once
+#include "common/common.h"
 #include <stdio.h>
+#include <array>
 
+/**
+ * 
+ * 
+ * General memory implementation. This is how we are emulating how memory is going to be read, written, and even accessed.
+ * 
+ * Struct should know and be aware of the following
+ * 
+ * - zero page
+ * - stack
+ * - memory data
+ * - the last 6 significant bytes, because since the 6502 is little endian architecture.
+ * 
+*/
 
-class Memory{
-    
+struct Memory{
+    static constexpr int TOTAL_MAX_MEMORY = 1024 * 64;
+
+    // allowing to intilize the memory to default
+    // As well, allowing us to load in memory to it's initial state.
+    void initialize();
+    void load(std::string filename);
+
+    std::array<Byte, 100> zeroPage;
+    std::array<Byte, 100> stack;
+    std::array<Byte, 6> lastSixSignifBytes;
+    std::array<Byte, TOTAL_MAX_MEMORY - 206> memoryData;
 };
