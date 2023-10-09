940a580a434e2ac22948bf2198a35e1acc6ba397
= <=>
2023-10-08 20:26:11 -0700

    Added to make function calls that will change the state of the emulation

--- 6502_Emulator/include/core/6502-Emulator.h
+++ 6502_Emulator/include/core/6502-Emulator.h
@@ -1,5 +1,27 @@
 #pragma once
 #include <stdio.h>
 #include <cstring>
+#include <algorithm>
+#include <common/Instruction.h>
 
-void ExecuteEmulation(uint32_t cycles);
+/**
+ * @brief 
+ * 
+ * Just want to initiate a state that can check if we want to turn on the emulator or not.
+ * 
+ * @param state 
+ */
+
+void startup(bool state);
+
+
+/**
+ * @brief 
+ * 
+ * 
+ * void ExecuteEmulation(uint32_t)
+ * - Starts the state of the emulation, where we input the amount of cycles it takes for a specific instruction to run.
+ * 
+ * @param cycles 
+ */
+void ExecuteEmulator(uint32_t cycles, Instruction*);
