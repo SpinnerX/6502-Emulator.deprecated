3553594ca61687bcb2135cca8e4b4b6120afd7d9
type <type>
2023-10-14 05:55:16 -0700

    Initial commit

--- 6502_Emulator/src/core/6502-Emulator.cpp
+++ 6502_Emulator/src/core/6502-Emulator.cpp
@@ -1,18 +1,22 @@
 #include <core/6502-Emulator.h>
 
 
-void startup(bool state){
+void bootup6502CPU(bool state){
     if (state){
-        std::cout << "Initiating 6502 Emulation!\n";
+        printf("Initiating 6502 Emulation!\n");
     }
 }
 
-void ExecuteEmulator(uint32_t cycles, Instruction* instruction){
-    std::cout << "Executing the Emulator\n";
-    // Basically what we should also do is compute how many cycles should be running, just so we can compute that into the cycles var.
-    // This allows the loop to run a specific amount until we finish running those instructions
-    while(cycles > 0){
-        std::cout << "Running cycles #" << cycles << "!!!\n";
-        cycles--;
+
+void ExecuteEmulator(uint32_t cycles, State state){
+    State cpuState = State::OFF;
+    
+    if(state == START){
+        cpuState = RUNNING;
+    }
+
+
+    while(cpuState == RUNNING){
+        printf("CPU Running....\n");
     }
 }
