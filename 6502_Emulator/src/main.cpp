ed47d532bc43836e48206a6348c224e5823e1e34
type <type>
2023-10-14 05:56:07 -0700

    Initial commit

--- 6502_Emulator/src/main.cpp
+++ 6502_Emulator/src/main.cpp
@@ -1,10 +1,12 @@
 #include <core/6502-Emulator.h>
+#include <common/CPUState.h>
 using namespace std;
 
 int main(){
+    CPUState state;
+    state.initialize();
 
-    startup(true);
-
-    ExecuteEmulator(10, new Instruction());
+    ExecuteEmulator(10, ON);
+    
     return 0;
 }
